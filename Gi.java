9578ce9616228cceda3f6af15b85894ccc87c284
Chelupuri Ganesh <ganeshganu2266@gmail.com>
2025-01-23 11:23:08 +0530

    for codes

---
+++ Gi.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Gi
+{
+    public static void main(String args[])
+    {
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int max1=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]>max1)
+            {
+                max1=a[i];
+            }
+            
+        }
+        System.out.println(max1);
+    }
+}
